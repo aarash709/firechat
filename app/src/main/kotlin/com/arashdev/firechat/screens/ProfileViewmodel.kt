ee2c5e5a3fe7037a0113334b15f0510e3d8c199c
Arash <68777571+arashebrahimzade@users.noreply.github.com>
2025-02-19 13:08:04 +0330

    adding profile editing

--- app/src/main/kotlin/com/arashdev/firechat/screens/ProfileViewmodel.kt
+++ app/src/main/kotlin/com/arashdev/firechat/screens/ProfileViewmodel.kt
@@ -2,15 +2,22 @@ package com.arashdev.firechat.screens
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.arashdev.firechat.model.User
 import com.arashdev.firechat.service.AuthService
 import com.arashdev.firechat.service.RemoteStorageService
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.stateIn
 import kotlinx.coroutines.launch
 import timber.log.Timber
 
 class ProfileViewmodel(private val auth: AuthService, firestore: RemoteStorageService) :
 	ViewModel() {
 
-	val currentUser = auth.currentUser
+	val currentUser = auth.currentUser.stateIn(
+		scope = viewModelScope,
+		started = SharingStarted.WhileSubscribed(5_000L),
+		initialValue = User()
+	)
 	val currentUserId = auth.currentUserId
 
 	fun linkAccount(email: String, password: String) {
