40f27523b2c17572aafba27b542f544e521c104d
Arash <68777571+arashebrahimzade@users.noreply.github.com>
2025-03-06 17:16:36 +0330

    add profile photo to contact list

--- app/src/main/kotlin/com/arashdev/firechat/screens/Contacts.kt
+++ app/src/main/kotlin/com/arashdev/firechat/screens/Contacts.kt
@@ -1,18 +1,24 @@
 package com.arashdev.firechat.screens
 
+import android.graphics.BitmapFactory
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material.icons.filled.Person
 import androidx.compose.material.icons.filled.Search
-import androidx.compose.material.icons.outlined.AccountCircle
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
@@ -24,8 +30,13 @@ import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.asImageBitmap
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.PreviewLightDark
 import androidx.compose.ui.unit.dp
@@ -102,11 +113,41 @@ fun UserItem(modifier: Modifier = Modifier, user: User, onContactClick: () -> Un
 				verticalAlignment = Alignment.CenterVertically,
 				horizontalArrangement = Arrangement.spacedBy(8.dp)
 			) {
-				Icon(
-					imageVector = Icons.Outlined.AccountCircle,
-					modifier = Modifier.size(50.dp),
-					contentDescription = "contact profile pic"
-				)
+				Surface(
+					onClick = { },
+					Modifier
+						.clip(CircleShape)
+						.size(50.dp)
+				) {
+					if (user.profilePhotoBase64.isNotEmpty()) {
+						val bitmap by remember(user.profilePhotoBase64) {
+							val array = android.util.Base64.decode(
+								user.profilePhotoBase64,
+								android.util.Base64.DEFAULT
+							)
+							mutableStateOf(
+								BitmapFactory.decodeByteArray(array, 0, array.size)
+									.asImageBitmap()
+							)
+						}
+						Image(
+							bitmap,
+							contentDescription = ""
+						)
+					} else {
+						Box(Modifier.background(Color.Gray), contentAlignment = Alignment.Center) {
+							Icon(
+								imageVector = Icons.Default.Person,
+								modifier = Modifier
+									.fillMaxSize()
+									.padding(8.dp),
+								contentDescription = null
+							)
+						}
+
+					}
+
+				}
 				Column(modifier = Modifier.padding(0.dp)) {
 					Text(text = user.name, fontWeight = FontWeight.Bold)
 					Text(text = user.userId, color = MaterialTheme.colorScheme.onSurfaceVariant)
